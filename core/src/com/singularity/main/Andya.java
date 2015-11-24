adecb0d1abaa4981e3bdc6931d9a475197bf5547
huaiyudavid <davidyangrocs@gmail.com>
2015-11-24 01:39:49 -0500

    ( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)
    Army of Pikachu MUAHAHAHAHAHAHA

--- core/src/com/singularity/main/Andya.java
+++ core/src/com/singularity/main/Andya.java
@@ -2,7 +2,7 @@ package com.singularity.main;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.loaders.ModelLoader;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.g3d.Environment;
@@ -11,18 +11,19 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.utils.Array;
 
 public class Andya extends ApplicationAdapter {
 	/*SpriteBatch batch;
 	Texture img;*/
-    public PerspectiveCamera cam;
-    public Model model;
-    public ModelInstance instance;
-    public ModelBatch modelBatch;
-    public Environment environment;
+	public PerspectiveCamera cam;
     public CameraInputController camController;
+    public ModelBatch modelBatch;
+    public AssetManager assets;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+    public Environment environment;
+    public boolean loading;
 	
 	@Override
 	public void create () {
@@ -35,38 +36,50 @@ public class Andya extends ApplicationAdapter {
 		modelBatch = new ModelBatch();
 		
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
+        cam.position.set(30f, 30f, 30f);
         cam.lookAt(0,0,0);
         cam.near = 1f;
         cam.far = 300f;
         cam.update();
         
-        ModelLoader loader = new ObjLoader();
-        model = loader.loadModel(Gdx.files.internal("pikachu.obj"));
-        instance = new ModelInstance(model);
-        
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
+        
+        assets = new AssetManager();
+        assets.load("pikachu.obj", Model.class);
+        loading = true;
 	}
+	
+	private void doneLoading() {
+        Model pika = assets.get("pikachu.obj", Model.class);
+        for (float x = -20f; x <= 20f; x += 10f) {
+            for (float z = -20f; z <= 20f; z += 10f) {
+                ModelInstance pikaInstance = new ModelInstance(pika);
+                pikaInstance.transform.setToTranslation(x, 0, z);
+                instances.add(pikaInstance);
+            }
+        }
+        loading = false;
+    }
 
 	@Override
 	public void render () {
-		camController.update();
-		
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		if (loading && assets.update())
+            doneLoading();
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		/*batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();*/
-		
-		modelBatch.begin(cam);
-        modelBatch.render(instance, environment);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instances, environment);
         modelBatch.end();
 	}
 	
 	@Override
     public void dispose () {
 		modelBatch.dispose();
-        model.dispose();
+        instances.clear();
+        assets.dispose();
     }
 }
