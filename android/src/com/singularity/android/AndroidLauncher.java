d91eb961273dd8cb539bd7d153cb7c929dacb392
huaiyudavid <davidyangrocs@gmail.com>
2015-11-23 23:11:01 -0500

    intial commit
    ♥‿♥

---
+++ android/src/com/singularity/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.singularity.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.singularity.Andya;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Andya(), config);
+	}
+}
