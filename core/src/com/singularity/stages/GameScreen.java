524e4bcac71de5f91d50debea784cf5927141ed9
venividivici440 <venividivici440@gmail.com>
2015-11-26 22:03:31 -0500

    Same as last commit
    Forgot to save something

--- core/src/com/singularity/stages/GameScreen.java
+++ core/src/com/singularity/stages/GameScreen.java
@@ -1,5 +1,57 @@
 package com.singularity.stages;
 
-public class GameScreen {
+import com.badlogic.gdx.Screen;
+
+public class GameScreen implements Screen{
+	
+	private PhysicalGameWorld physicalGameWorld;
+	private PhysicalWorldRenderer physicalWorldRenderer;
+	
+	public GameScreen() {
+		physicalGameWorld = new PhysicalGameWorld();
+		physicalWorldRenderer = new PhysicalWorldRenderer(physicalGameWorld);
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- core/src/com/singularity/stages/PhysicalWorldRenderer.java
+++ core/src/com/singularity/stages/PhysicalWorldRenderer.java
@@ -2,4 +2,9 @@ package com.singularity.stages;
 
 public class PhysicalWorldRenderer {
 
+	private PhysicalGameWorld physicalGameWorld;
+	
+	public PhysicalWorldRenderer (PhysicalGameWorld w) {
+		physicalGameWorld = w;
+	}
 }
