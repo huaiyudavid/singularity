09a2ce5850c3c5d6c80c50390442460b7cb159d8
venividivici440 <venividivici440@gmail.com>
2015-11-30 01:30:30 -0500

    Revert "Same as last commit"
    This reverts commit 524e4bcac71de5f91d50debea784cf5927141ed9.

--- core/src/com/singularity/stages/PhysicalWorldRenderer.java
+++ core/src/com/singularity/stages/PhysicalWorldRenderer.java
@@ -2,9 +2,4 @@ package com.singularity.stages;
 
 public class PhysicalWorldRenderer {
 
-	private PhysicalGameWorld physicalGameWorld;
-	
-	public PhysicalWorldRenderer (PhysicalGameWorld w) {
-		physicalGameWorld = w;
-	}
 }
