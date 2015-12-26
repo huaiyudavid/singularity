940dd274b3141a93ef821108a1e752c890158fb7
huaiyudavid <davidyangrocs@gmail.com>
2015-12-25 21:11:25 -0600

    meh

--- desktop/src/com/singularity/desktop/DesktopLauncher.java
+++ desktop/src/com/singularity/desktop/DesktopLauncher.java
@@ -9,9 +9,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Singularity";
-		new LwjglApplication(new SphereTest(), config);
 		config.width = 720;
 		config.height = 480;
-		new LwjglApplication(new Andya(), config);
+		new LwjglApplication(new SphereTest(), config);
 	}
 }
